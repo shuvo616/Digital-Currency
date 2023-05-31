8d70f416326a1413a59cbf2c2afb62b9375d270f
shuvo616 <shuvo616@gmail.com>
2023-05-31 19:10:25 +0600

    testimonial & working process section added

--- Js/script.js
+++ Js/script.js
@@ -15,3 +15,21 @@ hLink.forEach(link => {
         faSolid.classList.toggle('fa-xmark');
     })
 })
+
+// testimonial
+const userTexts = document.getElementsByClassName('user-text');
+const userPics = document.getElementsByClassName('user-pic');
+
+function showReview() {
+    for (userPic of userPics) {
+        userPic.classList.remove("active-pic");
+    }
+    for (userText of userTexts) {
+        userText.classList.remove("active-text");
+    }
+
+    let i = Array.from(userPics).indexOf(event.target);
+
+    userPics[i].classList.add('active-pic');
+    userTexts[i].classList.add('active-text');
+}
