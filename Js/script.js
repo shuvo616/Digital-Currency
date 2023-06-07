c9b91a23f652e8bebe270f839b386f6ce8423694
shuvo616 <shuvo616@gmail.com>
2023-06-07 15:45:27 +0600

    pricing section added

--- Js/script.js
+++ Js/script.js
@@ -1,20 +1,3 @@
-// Mobile menu
-const hamburger = document.querySelector('#hamburger');
-const menu = document.querySelector('#menu');
-const hLink = document.querySelectorAll('#hLink');
-const faSolid = document.querySelector(".fa-solid");
-
-hamburger.addEventListener("click", () => {
-    menu.classList.toggle('hidden');
-    faSolid.classList.toggle('fa-xmark');
-})
-
-hLink.forEach(link => {
-    link.addEventListener('click', () => {
-        menu.classList.toggle('hidden');
-        faSolid.classList.toggle('fa-xmark');
-    })
-})
 
 // testimonial
 const userTexts = document.getElementsByClassName('user-text');
@@ -33,3 +16,28 @@ function showReview() {
     userPics[i].classList.add('active-pic');
     userTexts[i].classList.add('active-text');
 }
+
+// Price toggle button
+
+const toggleBtn = document.getElementById('toggleBtn');
+
+const card_1_front = document.querySelector("#card_1_front");
+const card_1_back = document.querySelector("#card_1_back");
+
+const card_2_front = document.querySelector("#card_2_front");
+const card_2_back = document.querySelector("#card_2_back");
+
+const card_3_front = document.querySelector("#card_3_front");
+const card_3_back = document.querySelector("#card_3_back");
+
+toggleBtn.addEventListener('change', () => {
+    card_1_front.classList.toggle('-rotate-y-180');
+    card_1_back.classList.toggle('rotate-y-180');
+
+    card_2_front.classList.toggle('-rotate-y-180');
+    card_2_back.classList.toggle('rotate-y-180');
+
+    card_3_front.classList.toggle('-rotate-y-180');
+    card_3_back.classList.toggle('rotate-y-180');
+
+})
